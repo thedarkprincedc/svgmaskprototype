47d64bf7b9f9f6b341f83837c75f959d2a5eca21
thedarkprincedc <brettmosley@hotmail.com>
2018-09-18 01:06:25 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+(function(){
+    var title = $(".title");
+    var previewWindow = $(".preview-window");
+    var lastSelectedColor = null;
+    function onSelectionClicked(event){
+        setSelectedColor(event.target.textContent);    
+        $(this).addClass("color-is-selected");
+    }
+    function setSelectedColor(colorName){
+        $(".selected-color").html(colorName);
+        previewWindow.addClass(colorName);
+        $(".preview-window").removeClass(lastSelectedColor)
+        $(".color-is-selected").removeClass("color-is-selected")
+        lastSelectedColor = colorName;
+    }
+
+    $( document ).ready(function() {
+        title.html("fossil title");
+        setSelectedColor("Red-T09");
+        $(".color-selection").click(onSelectionClicked);
+    });
+}())
